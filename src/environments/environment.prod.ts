ce201f80360e74d0665da33905870172e306cd52
guilhermeB-gomes <140649008+guilhermeB-gomes@users.noreply.github.com>
2025-03-28 11:20:55 +0000

    o

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
+    authDomain: "aula-3e82a.firebaseapp.com",
+    projectId: "aula-3e82a",
+    storageBucket: "aula-3e82a.firebasestorage.app",
+    messagingSenderId: "258284807710",
+    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
+    measurementId: "G-X8L8GS7897"
+  },
+  production: true
+};
